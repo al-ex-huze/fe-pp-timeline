365643fab4a9b4f223858fcc49390ae55f39fbb2
Alex Hughes <al.ex.hughes@outlook.com>
2024-08-26 12:01:14 +0100

    adds authenticator component

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,36 @@ import "../src/styles/Content.css";
 
 import { Routes, Route } from "react-router-dom";
 
+import type { AppProps } from "next/app";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import outputs from "/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Home from "./components/home/Home";
 import Profile from "./components/profile/Profile";
 import Portfolio from "./components/portfolio/Portfolio";
 
-function App() {
-
+function App({ Component, pageProps }: AppProps) {
     return (
-        <div className="App">
-            <Header />
-            <Nav />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/portfolio" element={<Portfolio />} />
-            </Routes> 
-        </div>
-    )
+        <Authenticator>
+            {({ signOut, user }) => (
+                <div className="App">
+                    <Header />
+                    <Nav />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/portfolio" element={<Portfolio />} />
+                    </Routes>
+                </div>
+            )}
+        </Authenticator>
+    );
 }
 
 export default App;
