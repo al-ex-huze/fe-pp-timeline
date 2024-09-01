ab7f5b13bbe283d85b55f3a37e9cae5fd25e6334
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-01 15:34:25 +0100

    adds logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,20 @@ import "../styles/Header.css";
 const Header = () => {
     return (
         <div className="Header">
-            Header
+            <div className="Header__logo-bg-1">
+                <img className="Header__logo-1"
+                    src="https://alimageexbuckhuetzepub.s3.eu-north-1.amazonaws.com/ah2ffffff.png"
+                    alt="AH Logo"
+                />
+            </div>
+            <div className="Header__logo-bg-2">
+                <img className="Header__logo-2"
+                    src="https://alimageexbuckhuetzepub.s3.eu-north-1.amazonaws.com/ah2ffffff.png"
+                    alt="AH Logo"
+                />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Header;
